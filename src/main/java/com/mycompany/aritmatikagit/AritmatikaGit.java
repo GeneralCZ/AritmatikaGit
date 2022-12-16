1e617c78fc15854dd74b459f092b4a96abac814f
GeneralCZ <darariyafi09@gmail.com>
2022-12-16 08:32:07 +0700

    ini adalah inisiasi Projrct Aritmatika

---
+++ src/main/java/com/mycompany/aritmatikagit/AritmatikaGit.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.aritmatikagit;
+
+/**
+ *
+ * @author DARARI
+ */
+public class AritmatikaGit {
+
+    private double bilSatu;
+    private double bilDua;
+
+    public AritmatikaGit() {
+    }
+
+    public AritmatikaGit(double bilSatu, double bilDua) {
+        this.bilSatu = bilSatu;
+        this.bilDua = bilDua;
+    }
+
+    public double getBilSatu() {
+        return bilSatu;
+    }
+
+    public void setBilSatu(double bilSatu) {
+        this.bilSatu = bilSatu;
+    }
+
+    public double getBilDua() {
+        return bilDua;
+    }
+
+    public void setBilDua(double bilDua) {
+        this.bilDua = bilDua;
+    }
+
+    // Penjumlahan
+    public double Penjumlahan() {
+        return 0;
+    }
+
+    // Penguarangan
+    public double Pengurangan() {
+        return 0;
+    }
+
+    // Perkalian
+    public double Perkalian() {
+        return 0;
+    }
+
+    // Pembagian
+    public double Pembagian() {
+        return 0;
+    }
+
+}
